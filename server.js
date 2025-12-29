8668bf6bb93bbea00c30902f40ff508a60af0bad
bini-it <143047673+bini-it@users.noreply.github.com>
2025-12-29 21:16:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import blogRoutes from "./routes/blog.js";
 
 const app = express();
 app.use(json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
